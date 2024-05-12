1da64e4d3f64d767007b2039ef846fa1716efcbe
Refúgio Lifestyle <luisenriquegomesportugal@hotmail.com>
2024-05-12 18:26:24 -0300

    rollback de vez

--- src/services/useInscritoService.js
+++ src/services/useInscritoService.js
@@ -1,4 +1,4 @@
-import { equalTo, onValue, orderByChild, query, ref } from "firebase/database";
+import { equalTo, get, onValue, orderByChild, query, ref } from "firebase/database";
 import { useCallback, useState } from "react";
 import { firebaseDatabase } from "../configs/firebase";
 import { useParse } from "../hooks/useParse";
@@ -6,7 +6,7 @@ import { useParse } from "../hooks/useParse";
 export const useInscritoService = () => {
   const [loading, setLoading] = useState(false)
   const [inscrito, setInscrito] = useState(null)
-  const {parseFirebaseObject} = useParse();
+  const { parseFirebaseObject } = useParse();
 
 
   const search = useCallback(async cpf => {
@@ -15,12 +15,13 @@ export const useInscritoService = () => {
     let refer = ref(firebaseDatabase, `inscricoes`)
     let search = query(refer, orderByChild('cpf'), equalTo(cpf));
 
-    onValue(search, snapshot => {
+    let unSubscribe = onValue(search, snapshot => {
       let inscritoParsed = parseFirebaseObject(snapshot.val());
 
       setInscrito(inscritoParsed);
       setLoading(false);
-    })    
+      unSubscribe();
+    })
   }, [])
 
   return {
