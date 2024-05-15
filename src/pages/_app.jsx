ae3e5398c505589b2cce92cdb2a63325cd164877
Refúgio Lifestyle <luisenriquegomesportugal@hotmail.com>
2024-05-15 17:19:37 -0300

    headers

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,14 +6,37 @@ import "../configs/locale";
 import "./index.css";
 
 import PrimeReact from "primereact/api";
+import Head from 'next/head';
 PrimeReact.cssTransition = true;
 PrimeReact.inputStyle = 'outlined';
 PrimeReact.inputStyle = 'filled';
 
 function MyApp({ Component, pageProps }) {
-  return <div className="home">
-    <Component {...pageProps} />
-  </div>
+  return <>
+    <Head>
+
+      <meta name="description" content="Nosso 14º aniversário está chegando e para comemorar vem aí o SUNSET 2024" />
+      <meta name="keywords" content="Refúgio,Lifestyle,Sunset 2024" />
+      <meta property="og:title" content="Sunset 2024 :: Refúgio Lifestyle" />
+      <meta property="og:description" content="Nosso 14º aniversário está chegando e para comemorar vem aí o SUNSET 2024" />
+      <meta property="og:url" content="https://arefugio.com.br" />
+      <meta property="og:site_name" content="Sunset 2024 :: Refúgio Lifestyle" />
+      <meta property="og:locale" content="pt-BR" />
+      <meta property="og:image:url" content="https://arefugio.com.br/assets/images/logo-302x44.png" />
+      <meta property="og:image:width" content="302" />
+      <meta property="og:image:height" content="44" />
+      <meta property="og:type" content="website" />
+      <meta name="robots" content="noindex, follow, nocache" />
+      <meta name="color-scheme" content="light" />
+      <meta name="author" content="Refúgio Lifestyle" />
+      <link rel="author" href="https://arefugio.com.br" />
+      <link rel="icon" href="https://arefugio.com.br/assets/images/favicon-dark.png" media="(prefers-color-scheme: light)" />
+      <link rel="icon" href="https://arefugio.com.br/assets/images/favicon-light.png" media="(prefers-color-scheme: dark)" />
+    </Head>
+    <div className="home">
+      <Component {...pageProps} />
+    </div>
+  </>
 }
 
 export default MyApp
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,10 @@ import { useParse } from '../hooks/useParse';
 import { classNames } from 'primereact/utils';
 import { cpf } from 'cpf-cnpj-validator'
 
+export const metadata = {
+  title: 'Sunset 2024 :: Refúgio Lifestyle'
+};
+
 export default function Index() {
   const [stepForm, setStepForm] = useState(1);
   const { redes, celulas, setRedeRef, } = useRedesService()
@@ -121,23 +125,6 @@ export default function Index() {
   return <>
     <Head>
       <title>{metadata.title}</title>
-      <meta name="description" content="Nosso 14º aniversário está chegando e para comemorar vem aí o SUNSET 2024" />
-      <meta name="keywords" content="Refúgio,Lifestyle,Sunset 2024" />
-      <meta property="og:title" content="Sunset 2024 :: Refúgio Lifestyle" />
-      <meta property="og:description" content="Nosso 14º aniversário está chegando e para comemorar vem aí o SUNSET 2024" />
-      <meta property="og:url" content="https://arefugio.com.br" />
-      <meta property="og:site_name" content="Sunset 2024 :: Refúgio Lifestyle" />
-      <meta property="og:locale" content="pt-BR" />
-      <meta property="og:image:url" content="https://arefugio.com.br/assets/images/logo-302x44.png" />
-      <meta property="og:image:width" content="302" />
-      <meta property="og:image:height" content="44" />
-      <meta property="og:type" content="website" />
-      <meta name="robots" content="noindex, follow, nocache" />
-      <meta name="color-scheme" content="light" />
-      <meta name="author" content="Refúgio Lifestyle" />
-      <link rel="author" href="https://arefugio.com.br" />
-      <link rel="icon" href="https://arefugio.com.br/assets/images/favicon-dark.png" media="(prefers-color-scheme: light)" />
-      <link rel="icon" href="https://arefugio.com.br/assets/images/favicon-light.png" media="(prefers-color-scheme: dark)" />
     </Head>
     <main className='flex flex-col lg:flex-row justify-evently gap-12'>
       <section className='flyer'>
